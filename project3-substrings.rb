a0bb7e2c32f32b0f8aecd89bb09df11a06adb74b
Jennifer G <jennifer.garza2691@gmail.com>
2018-09-29 15:45:58 +0000

    Trying mohammads code

--- project3-substrings.rb
+++ project3-substrings.rb
@@ -1,10 +1,34 @@
+def substrings(sentence, dict)
+  hash = Hash.new(0)
+
+  str_arr = sentence.downcase.split(' ')
+
+  str_arr.each do |word|
+    dict.each do |substr|
+      hash[substr] += 1 if word.include?(substr)
+    end
+  end
+
+  puts hash
+end
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+substrings('below', dictionary)
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+=begin
 > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 def substrings (word,dictionary)
     dictionary.include?(word)
-=begin
+
   > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
   => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
   > substrings("below", dictionary)
   => { "below" => 1, "low" => 1 }
   =end
+ # mohammad code from solutions 
+ 
+ 
   
