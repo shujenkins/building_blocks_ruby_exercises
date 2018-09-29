87bfba462203f45b2a7c95f13b7ea75c923bdda6
Jennifer G <jennifer.garza2691@gmail.com>
2018-09-29 15:30:24 +0000

    noted the project notes, Unsure what to do

--- project3-substrings.rb
+++ project3-substrings.rb
@@ -1,4 +1,10 @@
 > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 def substrings (word,dictionary)
     dictionary.include?(word)
-    
+=begin
+  > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  > substrings("below", dictionary)
+  => { "below" => 1, "low" => 1 }
+  =end
+  
